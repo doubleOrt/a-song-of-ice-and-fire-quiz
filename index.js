af903dd21651f395902fbfead20f54c6b2922fe3
unknown <fantasybl8@gmail.com>
2018-03-20 22:38:49 +0300

    Was supposed to be: Replaces the 'shuffleQuestionsAndOptions' method with a 'shuffleOptions' method. It is cleaner this way since shuffling questions only requires a single function call and an assignment. But I did not realize I had made tens of other uncomitted changes as well, so this is a total-mess commit.

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 
 (function($) {
 
-  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  const shuffleArray = function shuffleArray(arr) {
+    let currentIndex = arr.length;
+    let temporaryValue;
+    let randomIndex;
     // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+    while (currentIndex !== 0) {
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
       // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+      temporaryValue = arr[currentIndex];
+      arr[currentIndex] = arr[randomIndex];
+      arr[randomIndex] = temporaryValue;
     }
-    return array;
-  }
+    return arr;
+  };
 
-  const markUp = {
-    questionForm: function getQuestionForm(question) {
+  const quiz = (function() {
+    const markUp = {
+      getQuestionForm: function getQuestionForm(question) {
         let markUp =
           `<div class='quiz-div quiz-col'>
           <form class='main-quiz-form row'>
@@ -25,7 +28,7 @@
             <p class='quiz-questions'>${question.question}</p>
             <div>`;
 
-        question.choices.forEach( (answer, index) => {
+        question.options.forEach( (answer, index) => {
           markUp +=
             `<div class="answer-to-question">
               <input type="radio" id=${answer}
@@ -33,14 +36,14 @@
               ${ index == 0 ? 'required' : null } >
               <label for=${answer}>${answer}</label>
             </div>`;
-        } );
+          } );
 
         markUp +=
-        `<div class="answer-to-question hidden">
-              <input class ='the-correct-answer' type="radio" id='is${question.answer}'
-              name="answer" value=${question.answer}>
-              <label for=${question.answer}>${question.answer}</label>
-            </div>
+          `<div class="answer-to-question hidden">
+          <input class ='the-correct-answer' type="radio" id='is${question.answer}'
+          name="answer" value=${question.answer}>
+          <label for=${question.answer}>${question.answer}</label>
+          </div>
           </div>
           </div>
           <div class='submit-div'>
@@ -53,183 +56,172 @@
           <div class='current-score-div'>
 
           </div>
-        </div>
-        </form>
-        </div> `;
+          </div>
+          </form>
+          </div> `;
 
         return markUp;
-    },
-    quizResults: function quizResults(score, total) {
-      return `<div class='row final-results'>
-      <p class='final-score-paragraph'>
-      Your final score is ${score} out of ${total}.
-      <p>
-      <button class="start-over final-score-button">Play Again</button>
-      </div>`;
-    },
-    incorrectAnswerPara: function incorrectAnswerPara(correctAnswer) {
-      return `<p class='incorrect-text'>
-      You should have chosen ${correctAnswer}.
-      </p>`;
-    },
-  };
+      },
+      getQuizResults: function getQuizResults(score, total) {
+        return `<div class='row final-results'>
+        <p class='final-score-paragraph'>
+        Your final score is ${score} out of ${total}.
+        <p>
+        <button class="start-over final-score-button">Play Again</button>
+        </div>`;
+      },
+      getIncorrectAnswerPara: function getIncorrectAnswerPara(correctAnswer) {
+        return `<p class='incorrect-text'>
+        You should have chosen ${correctAnswer}.
+        </p>`;
+      },
+    };
 
-  //This function will check to see if the answer is right or wrong and return that info.
-  //Depending on if the answer was right or wrong, a different pop-up image will be displayed.
-  function evaluateAnswer(question, userAnswer) {
-    if (question.answer === userAnswer) {
-      return true;
-    }
-    return false;
-  }
-
-  // side-effecting functions:
-
-  function setQuestionsCountText(num, total) {
-    $('.current-question-div').text(
-      `Question: ${num}/${total}`
-    );
-  }
-
-  function setScoreCountText(num, total) {
-    $('.current-score-div').text(
-      `Score: ${num}/${total}`
-    );
-  }
-
-  function showScore(num, total) {
-    $('main').append(
-      markUp.quizResults(num, total)
-     );
-  }
-
-  function addQuestion(question) {
-    $('main').append(
-      markUp.questionForm( question )
-    );
-  }
-
-  function showPopUp(answerIsCorrect, correctAnswer) {
-    $('main, header').addClass('body-transparent');
-    if (answerIsCorrect) {
-      $('.pop-outer-correct').fadeIn();
-    } else {
-      $('.pop-inner-incorrect').append(
-        markUp.incorrectAnswerPara(correctAnswer)
-      );
-      $('.pop-outer-incorrect').fadeIn();
-    }
-  }
+    return {
+      start: function start() {
+        $('.start-page-div').addClass('hide-start-page');
+      },
+      reset: function reset() {
+        $('main').empty();
+        $('.start-page-div').removeClass('hide-start-page');
+      },
+      setQuestionCount: function setQuestionCount(num, total) {
+        $('.current-question-div').text(
+          `Question: ${num}/${total}`
+        );
+      },
+      setScoreCount: function setScoreCount(num, total) {
+        $('.current-score-div').text(
+          `Score: ${num}/${total}`
+        );
+      },
+      appendQuestion: function appendQuestion(question) {
+        $('main').append(
+          markUp.getQuestionForm( question )
+        );
+      },
+      displayResults: function displayResults() {
+        $('main').append(
+          markUp.getQuizResults(num, total)
+         );
+      },
+      displayPopUp: function displayPopUp(correctAnswer) {
+        $('main, header').addClass('body-transparent');
+        if (!correctAnswer) {
+          $('.pop-outer-correct').fadeIn();
+        } else {
+          $('.pop-outer-incorrect')
+            .fadeIn()
+            .find('.pop-inner-incorrect')
+            .append( markUp.getIncorrectAnswerPara(correctAnswer) );
+        }
+      },
+    };
+  })();
 
   (function() {
     //For a quiz app, we have an array of objects.
     //Each object contains the question, an array of the possible answers, and the correct answer.
-    let questionsAndAnswers = [
+    let questions = [
       {
         question: "Which one of Daenerys' handmaidens died in the red waste prior to arriving in Qarth?",
-        choices: ['Irri', 'Doreah', 'Jhiqui', 'Missandei'],
+        options: ['Irri', 'Doreah', 'Jhiqui', 'Missandei'],
         answer: 1
       },
       {
         question: "Which one of the nine free cities of Essos was not originally a colony of Old Valyria?",
-        choices: ['Pentos', 'Bravoos', 'Volantis', 'Myr'],
+        options: ['Pentos', 'Bravoos', 'Volantis', 'Myr'],
         answer: 1
       },
       {
         question: "Who forged Robert Baratheon's warhammer?",
-        choices: ['Ironbelly', 'Mikken', 'Donal Noye', 'Tobho Mott'],
+        options: ['Ironbelly', 'Mikken', 'Donal Noye', 'Tobho Mott'],
         answer: 2
       },
       {
         question: "During one of Arya's dancing lessons, Syrio Forel talks of an exotic land with lizards with scythes for claws. What continent is he alluding to?",
-        choices: ['Sorthoryos', 'Ulthos', 'Great Moraq', 'Essos'],
+        options: ['Sorthoryos', 'Ulthos', 'Great Moraq', 'Essos'],
         answer: 0
       },
       {
         question: "Who does Eddard Stark deem the greatest warrior he ever encountered?",
-        choices: ['Barristan Selmy', 'Gerold Hightower', 'Oswell Whent', 'Arthur Dayne'],
+        options: ['Barristan Selmy', 'Gerold Hightower', 'Oswell Whent', 'Arthur Dayne'],
         answer: 3
       },
       {
         question: "What house is home to Greywater Watch?",
-        choices: ['House Karstark', 'House Reed', 'House Glover', 'House Talhart'],
+        options: ['House Karstark', 'House Reed', 'House Glover', 'House Talhart'],
         answer: 1
       },
       {
         question: "Who died at the hands of Catelyn Stark during the red wedding?",
-        choices: ['Aegon Frey', 'Roslin Frey', 'Joyeuse Erenford', 'Ryman Frey'],
+        options: ['Aegon Frey', 'Roslin Frey', 'Joyeuse Erenford', 'Ryman Frey'],
         answer: 0
       },
       {
         question: "Who attempted to kill Myrcella Baratheon in the midst of Arianne Martell's failed plan to put her on the iron throne?",
-        choices: ['Doran Martell', 'Areo Hotah', 'Gerold Dayne', 'Quentyn Martell'],
+        options: ['Doran Martell', 'Areo Hotah', 'Gerold Dayne', 'Quentyn Martell'],
         answer: 2
       },
       {
         question: "Who won the melee at the hand's tourney in King's Landing?",
-        choices: ['Beric Dondarrion', 'Loras Tyrell', 'Thoros of Myr', 'Balon Swann'],
+        options: ['Beric Dondarrion', 'Loras Tyrell', 'Thoros of Myr', 'Balon Swann'],
         answer: 2
       },
       {
         question: "What are the Lannister's house words?",
-        choices: ['Growing strong', 'Hear me roar', 'A Lannister always pays his debts', 'We do not sow'],
+        options: ['Growing strong', 'Hear me roar', 'A Lannister always pays his debts', 'We do not sow'],
         answer: 1
       }
     ];
-    questionsAndAnswers.shuffleEverything = function() {
-      // can't re-assign this so decided to just refer to the variable directly everywhere to avoid confusion
-      questionsAndAnswers = shuffle(questionsAndAnswers);
-      questionsAndAnswers.forEach( (el) => {
-        const correctAnswer = el.choices[el.answer];
-        el.choices = shuffle(el.choices);
-        el.answer = el.choices.indexOf(correctAnswer);
-      } )
+    questions.shuffleOptions = () => {
+      questions.forEach( el => {
+        const correctAnswer = el.options[el.answer];
+        el.options = shuffleArray(el.options);
+        el.answer = el.options.indexOf(correctAnswer);
+      } );
     };
 
-    questionsAndAnswers.shuffleEverything();
+    questions = shuffleArray(questions);
+    questions.shuffleOptions();
 
-    let currentQuestionIndex = 0;
-    let currentScore = 0;
+    let questionIndex = 0;
+    let score = 0;
 
-    function startQuiz() {
-      $('.start-page-div').addClass('hide-start-page');
-      addQuestion( questionsAndAnswers[currentQuestionIndex] );
-    }
-
-    function resetQuiz() {
-      questionsAndAnswers.shuffleEverything();
-      currentQuestionIndex = 0;
-      currentScore = 0;
-      $('main').empty();
-      $('.start-page-div').removeClass('hide-start-page');
-    }
-
-    $(document).on('submit', '.start-page-form', (e) => {
+    $(document).on('submit', '.start-page-form', e => {
       e.preventDefault();
-      startQuiz();
+      quiz.appendQuestion( questions[questionIndex] );
+      quiz.setScoreCount( score, questions.length );
+      quiz.setQuestionCount( questionIndex, questions.length );
+      quiz.start();
     });
 
-    $(document).on('click', '.start-over', resetQuiz);
+    $(document).on('click', '.start-over', () => {
+      questionIndex = 0;
+      score = 0;
+      questions = shuffleArray(questions);
+      questions.shuffleOptions();
+      quiz.reset();
+    });
 
     $(document).on( 'submit', '.main-quiz-form', (e) => {
       e.preventDefault();
-      const question = questionsAndAnswers[currentQuestionIndex];
-      const answerIsCorrect = evaluateAnswer(
-        question,
-        Number( $('.answer-to-question input:checked').attr('data-index') )
-      );
-      showPopUp( answerIsCorrect, question.choices[question.answer] );
-      answerIsCorrect ? currentScore++ : null;
-      setScoreCountText(currentScore, questionsAndAnswers.length);
-
-      currentQuestionIndex++;
-      setQuestionsCountText(currentQuestionIndex);
-      $('.quiz-div').remove();
-      if (currentQuestionIndex < questionsAndAnswers.length) {
+      const answerIsCorrect = questions[questionIndex].answer === Number(
+          $('.answer-to-question input:checked').attr('data-index')
+        );
+      quiz.displayPopUp(
+        !answerIsCorrect ? questions[questionIndex].options[questions[questionIndex].answer] : null
+       );
+      answerIsCorrect ? score++ : null;
+      questionIndex++;
+      if (questionIndex < questions.length) {
+        $('.quiz-div').remove();
         // careful! this and the 'questions' variable refer to different things by now, that's why we are not using it here as well.
-        addQuestion( questionsAndAnswers[currentQuestionIndex] );
+        quiz.appendQuestion( questions[questionIndex] );
+        quiz.setScoreCount( score, questions.length );
+        quiz.setQuestionCount( questionIndex, questions.length );
       } else {
-        showScore( currentScore, questionsAndAnswers.length );
+        quiz.displayResults( score, questions.length );
       }
     });
 
